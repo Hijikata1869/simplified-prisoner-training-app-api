0b794b94356e1dd223de95b7cc2e2fa8fc833880
Hijikata1869 <liv.knk@gmail.com>
2023-08-30 00:35:48 +0900

    corsの設定を変更

--- Gemfile
+++ Gemfile
@@ -54,3 +54,9 @@ gem "pry-rails"
 gem 'redis-actionpack'
 gem "redis", "~> 4.8.1", "< 5"
 
+
+gem "dockerfile-rails", ">= 1.5", :group => :development
+
+gem "sentry-ruby", "~> 5.10"
+
+gem "sentry-rails", "~> 5.10"
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000", "https://simplified-prisoner-training-app-frontend-5gdn7zire.vercel.app/"
 
     resource "*",
       headers: :any,
